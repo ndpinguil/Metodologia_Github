8945f21e56a1b3034873b6a2a54a305897905c53
ndpinguil <116099207+ndpinguil@users.noreply.github.com>
2023-02-03 09:52:05 -0500

    commit

---
+++ ACS15_sunstring/ejercicio1.js
@@ -0,0 +1,4 @@
+var cadena = 'Ecuador es un pais que esta en America';
+for (var i = 0 ; i < cadena.length ; i++){
+console.log( cadena.substring(i, (cadena.length)) );
+}
---
+++ ACS15_sunstring/ejercicio2.js
@@ -0,0 +1,4 @@
+var cadena = 'Ela vida es bella como una pizza';
+for (var i = 0 ; i < cadena.length ; i++){
+console.log( cadena.substring(i, (cadena.length)) );
+}
